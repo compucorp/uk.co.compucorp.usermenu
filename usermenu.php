152cf35b9368d02cc3e5dc333d809f4fcb47a034
reneolivo <minet.ws@gmail.com>
2020-03-05 22:46:55 -0400

    CATL-1237: Remove unnecessary comment block

--- usermenu.php
+++ usermenu.php
@@ -141,34 +141,6 @@ function usermenu_civicrm_themes(&$themes) {
   _usermenu_civix_civicrm_themes($themes);
 }
 
-// --- Functions below this ship commented out. Uncomment as required. ---
-
-/**
- * Implements hook_civicrm_preProcess().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
- *
-function usermenu_civicrm_preProcess($formName, &$form) {
-
-} // */
-
-/**
- * Implements hook_civicrm_navigationMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
- *
-function usermenu_civicrm_navigationMenu(&$menu) {
-  _usermenu_civix_insert_navigation_menu($menu, 'Mailings', array(
-    'label' => E::ts('New subliminal message'),
-    'name' => 'mailing_subliminal_message',
-    'url' => 'civicrm/mailing/subliminal',
-    'permission' => 'access CiviMail',
-    'operator' => 'OR',
-    'separator' => 0,
-  ));
-  _usermenu_civix_navigationMenu($menu);
-} // */
-
 /**
  * Implements hook_civicrm_alterContent().
  * Adds extra settings fields to the Civicase Admin Settings form.
