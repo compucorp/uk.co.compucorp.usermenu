41b5d99ea74e427dac1635deda45ede761b29230
Rene Olivo <rene.olivo.do@gmail.com>
2020-04-14 15:59:38 +0100

    CATL-1310: Implement base core resource list hook on Home Menu class

--- CRM/Usermenu/Hooks/CoreResourceList/HomeMenu.php
+++ CRM/Usermenu/Hooks/CoreResourceList/HomeMenu.php
@@ -1,18 +1,26 @@
 <?php
 
+use CRM_Usermenu_Hooks_CoreResourceList_Base as BaseHook;
+
 /**
  * Home Menu Core ResourceList hook.
  */
-class CRM_Usermenu_Hooks_CoreResourceList_HomeMenu {
+class CRM_Usermenu_Hooks_CoreResourceList_HomeMenu extends BaseHook {
 
-  /** Home Menu Item */
+  /**
+   * Home Menu Item.
+   *
+   * @var object
+   */
   private $homeMenu;
 
   /**
-   * Appends the home menu JS asset and adds the home menu navigation data to
-   * the global settings object so it can be accessed by JS.
+   * Appends the home menu JS asset.
+   *
+   * It also adds the home menu navigation data to the global settings object
+   * so it can be accessed by JS.
    */
-  public function run () {
+  protected function appendResources() {
     $homeMenu = $this->getHomeMenu();
 
     CRM_Core_Resources::singleton()
@@ -28,24 +36,25 @@ class CRM_Usermenu_Hooks_CoreResourceList_HomeMenu {
    * Runs when appending assets to the HTML header region, and when the
    * Home Menu navigation item is set to active.
    *
-   * @param Array $items
-   * @param String $region
-   * @return Bool
+   * @return bool
+   *   True when the hook should run.
    */
-  public function shouldRun(array &$items, $region) {
+  protected function shouldRun() {
     $homeMenu = $this->getHomeMenu();
-    $isHeaderRegion = $region === 'html-header';
+    $isHeaderRegion = $this->region === 'html-header';
     $isUserMenuActive = $homeMenu && $homeMenu['is_active'] == 1;
 
     return $isHeaderRegion && $isUserMenuActive;
   }
 
   /**
-   * Returns the Home Menu navigation item. Stores it in a local variable to avoid
-   * repeated calls to the database. Returns NULL when the navigation item is not
-   * found.
+   * Returns the Home Menu navigation item.
    *
-   * @return Array|Null
+   * It Stores it in a local variable to avoid repeated calls to the database.
+   * Returns NULL when the navigation item is not found.
+   *
+   * @return array|null
+   *   The home menu item or null.
    */
   private function getHomeMenu() {
     if (!$this->homeMenu) {
@@ -62,4 +71,3 @@ class CRM_Usermenu_Hooks_CoreResourceList_HomeMenu {
   }
 
 }
-
