88deaab45445a0f3b27ae474c2986c48f50a0460
Muhammad Shahrukh <>
2024-07-29 12:59:22 +0500

    COMCL-635: Upgrade civix

--- CRM/Usermenu/Upgrader.php
+++ CRM/Usermenu/Upgrader.php
@@ -3,7 +3,7 @@
 /**
  * Collection of upgrade steps.
  */
-class CRM_Usermenu_Upgrader extends CRM_Usermenu_Upgrader_Base {
+class CRM_Usermenu_Upgrader extends CRM_Extension_Upgrader_Base {
 
   /**
    * Runs install scripts.
